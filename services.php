ef82205368df690a71246f0f059e6f450911fa23
Mathilde Arconte <signup@marconte.fr>
2021-02-09 14:43:03 +0100

    modal services

--- services.php
+++ services.php
@@ -41,14 +41,16 @@
 									$query->the_post();
 
 									$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-									<div>
+									<div class="service" data-toggle="modal" data-target="#modal-<?=$post->ID ?>">
 
 									<?php if($image) :?>
-										<img src="<?php echo $image[0]?>" class="card-blog_img img-shadow atelier__img" alt="<?php the_title(); ?>"/>
+										<img src="<?php echo $image[0]?>" class="card-blog_img img-shadow service__img"
+										alt="<?php the_title(); ?>"
+										 />
 									<?php else : ?>
 										<img src="<?php echo get_template_directory_uri() ; ?>/assets/images/default_image.png" class="card-blog_img img-shadow atelier__img" alt="<?php the_title(); ?>"/>
 									<?php endif; ?>
-									<p class="atelier__title"><?php the_title(); ?></p>
+									<p class="service__title"><?php the_title(); ?></p>
 									</div>
 							<?php  endwhile;	wp_reset_postdata();  endif; ?>
 						</div>
@@ -89,4 +91,51 @@
 	</section>
 	<?php endif; ?>
 </main>
+
+<!-- Modal -->
+<?php
+	$args = array(
+		'post_type' => 'atelier',
+		'posts_per_page' => 4
+	);
+	$query = new WP_Query( $args );
+	if ( $query->have_posts() ) :
+		while ( $query->have_posts() ) :
+			$query->the_post();
+			$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+			?>
+<div class="modal fade" id="modal-<?=$post->ID ?>" tabindex="-1" aria-labelledby="modal-<?=$post->ID ?>" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+       <button type="button" class="close btn-icon" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+		<div class="modal-header">
+		</div>
+      <div class="modal-body">
+        <div class="row">
+			<div class="col-sm-3">
+				<img src="<?= $image[0] ?>" alt="" class="img-shadow img-radius img-fluid mb-5">
+				<div class="services_list">
+					<ul>
+						<?php $details = get_field('details');?>
+						<li class="services_list_item"><?= $details['people'] ?></li>
+						<li class="services_list_item"><?= $details['time']?></li>
+						<li class="services_list_item"><?= $details['type']?></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-sm-9 pl-5">
+				<h2 class="title"><?php the_title(); ?></h2>
+				<?php if ( get_field('desc') ) : ?>
+					<p class="desc"><?php echo get_field('desc'); ?></p>
+				<?php endif; ?>
+				<a href="" class="btn btn-primary">Je m'inscris</a>
+			</div>
+		</div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php endwhile; wp_reset_postdata(); endif; ?>
 <?php get_footer(); ?>
