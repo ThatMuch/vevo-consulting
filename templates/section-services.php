fd750abeef84ccf62998fc4ddac6975921de7714
Mathilde Arconte <signup@marconte.fr>
2026-02-12 15:35:48 +0100

    fix: Update PHP short tags, improve image attribute fallbacks with null coalescing, and correct ternary operator grouping.

--- templates/section-services.php
+++ templates/section-services.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 /**
  * Services Block
@@ -12,7 +12,7 @@
  */
 ?>
 <?php $fond = get_sub_field('fond'); ?>
-<section class="section section-services <?php echo  ($fond == "Primary") ? "bg-primary": ($fond == "Gris") ? "bg-light" : "" ?>">
+<section class="section section-services <?php echo ($fond == "Primary") ? "bg-primary" : (($fond == "Gris") ? "bg-light" : "") ?>">
       <div class="container">
             <!-- Title -->
             <?php if (get_sub_field('title')) : ?>
@@ -25,33 +25,39 @@
                   if (have_rows('service')) :  $i = 0; ?>
                         <?php while (have_rows('service')) : the_row(); ?>
                               <div class="<?php echo $i == 0 ? "col-lg-6" : "col-lg-3" ?>  col-md-12 section-services__item">
-                              <a href="<?php if ( get_sub_field('link') ) : $link = get_sub_field('link'); echo  $link; endif; ?> ">
-                                    <!-- Image -->
-                                    <?php if (get_sub_field('image')) : $img = get_sub_field('image'); ?>
-                                    <div class="img-wrapper">
-                                          <img class="section-services__item__image img-fluid img-radius" data-src="<?php echo  $img['sizes']['large'] ?>" alt="<?php echo  $img['alt']; ?>">
-                                    </div>
-                                    <?php endif; ?>
-                                    <!-- Image -->
-                                                <!-- Icon title -->
-                                                <?php if (get_sub_field('title')) : ?>
-                                                      <div class="section-services__item__title-wrapper">
-                                                            <h3 class="section-services__item__title">
+                                    <a href="<?php if (get_sub_field('link')) : $link = get_sub_field('link');
+                                                      echo  $link;
+                                                endif; ?> ">
+                                          <!-- Image -->
+                                          <?php if (get_sub_field('image')) : $img = get_sub_field('image'); ?>
+                                                <div class="img-wrapper">
+                                                      <img class="section-services__item__image img-fluid img-radius" data-src="<?php echo  $img['sizes']['large'] ?? $img['url'] ?? '' ?>" alt="<?php echo  $img['alt'] ?? ''; ?>">
+                                                </div>
+                                          <?php endif; ?>
+                                          <!-- Image -->
+                                          <!-- Icon title -->
+                                          <?php if (get_sub_field('title')) : ?>
+                                                <div class="section-services__item__title-wrapper">
+                                                      <h3 class="section-services__item__title">
 
-                                                                        <?php echo  get_sub_field('title'); ?>
+                                                            <?php echo  get_sub_field('title'); ?>
 
-                                                            </h3>
-                                                      </div>
-                                                <?php endif; ?>
-                                                <!-- Icon title -->
-                                                <!-- Texte -->
-                                            <!--     <?php //if (get_sub_field('text')) : ?>
-                                                      <p class="section-services__item__text"> <?php // echo  get_sub_field('text'); ?></p>
-                                                <?php // endif; ?> -->
-                                                <!-- Texte -->
-                                                      </a>
+                                                      </h3>
+                                                </div>
+                                          <?php endif; ?>
+                                          <!-- Icon title -->
+                                          <!-- Texte -->
+                                          <!--     <?php //if (get_sub_field('text')) : 
+                                                      ?>
+                                                      <p class="section-services__item__text"> <?php // echo  get_sub_field('text'); 
+                                                                                                ?></p>
+                                                <?php // endif; 
+                                                ?> -->
+                                          <!-- Texte -->
+                                    </a>
                               </div>
-                        <?php $i++; endwhile; ?>
+                        <?php $i++;
+                        endwhile; ?>
                   <?php endif; ?>
                   <!-- Service -->
             </div>
